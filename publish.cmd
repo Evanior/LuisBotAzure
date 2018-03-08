ac9850fed057afa002200a91194099f5d208a7ab
tim <tim@Tim-PC.wat.hegyd.net>
2018-03-08 14:37:25 +0100

    init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=timimieluisbot-Web-Deploy.pubxml -p:Password=QhNtdbaGs3Gm4n3zFhtxe7PwHhAlElcKw0ptKNNgtr9tr2woZnqjjjzu3zKM
+
